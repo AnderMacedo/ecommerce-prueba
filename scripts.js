d1fae391817246f6db3d765c2c3c1daa0b0c4d9b
AnderMacedo <u20201c179@upc.edu.pe>
2025-04-28 04:12:57 -0500

    Agregando los archivos del proyecto ecommerce

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+// Placeholder script for product functionality
+
+const buttons = document.querySelectorAll('.product-card button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Item added to cart');
+  });
+});
